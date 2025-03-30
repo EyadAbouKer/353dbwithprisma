de531590b4a6e7c8104cb12e208c871057e1457a
EyadAbouKer <aboukereyad@gmail.com>
2025-03-30 19:07:01 -0400

    Family Member Layout

---
+++ src/components/inputforms/RegisterFamilyMember.jsx
@@ -0,0 +1,332 @@
+"use client"
+import { useState } from "react"
+import { toast } from "sonner"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { format } from "date-fns"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Calendar } from "@/components/ui/calendar"
+import { Calendar as CalendarIcon } from "lucide-react"
+
+const formSchema = z.object({
+  FirstName: z.string().min(1),
+  LastName: z.string().min(1),
+  LocationID: z.number().min(0).max(9999).optional(),
+  Status: z.string().min(1).optional(),
+  DOB: z.coerce.date().optional(),
+  SIN: z.number().min(0).max(999999999),
+  MedicareNumber: z.string().min(1),
+  Email: z.string().optional(),
+  Address: z.string().min(1).optional(),
+  City: z.string().min(1).optional(),
+  Province: z.string().min(1).optional(),
+  clubmembers: z.string().min(1),
+  familyrelationships: z.string().min(1).optional()
+});
+
+export default function RegisterFamilyMember() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      DOB: new Date()
+    },
+  });
+
+  function onSubmit(values) {
+    try {
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-6">
+            <FormField
+              control={form.control}
+              name="FirstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>FirstName</FormLabel>
+                  <FormControl>
+                    <Input 
+                      placeholder="FirstName*"
+                      {...field} 
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="col-span-6">
+            <FormField
+              control={form.control}
+              name="LastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>LastName</FormLabel>
+                  <FormControl>
+                    <Input 
+                      placeholder="LastName"
+                      type="text"
+                      {...field} 
+                    />
+                  </FormControl>
+                  <FormDescription>LastName</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-4">
+            <FormField
+              control={form.control}
+              name="LocationID"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>LocationID</FormLabel>
+                  <FormControl>
+                    <Input 
+                      placeholder="LocationID"
+                      type="number"
+                      {...field} 
+                    />
+                  </FormControl>
+                  <FormDescription>LocationID</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+        <FormField
+          control={form.control}
+          name="Status"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Status</FormLabel>
+              <FormControl>
+                <Input 
+                  placeholder="Status"
+                  {...field} 
+                />
+              </FormControl>
+              <FormDescription>Status</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="DOB"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>DOB</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="SIN"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>SIN</FormLabel>
+              <FormControl>
+                <Input 
+                  placeholder="SIN"
+                  type="number"
+                  {...field} 
+                />
+              </FormControl>
+              <FormDescription>SIN</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="MedicareNumber"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>MedicareNumber</FormLabel>
+              <FormControl>
+                <Input 
+                  placeholder="MedicareNumber"
+                  {...field} 
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="Email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input 
+                  placeholder="Email"
+                  type="email"
+                  {...field} 
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-4">
+            <FormField
+              control={form.control}
+              name="Address"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Address</FormLabel>
+                  <FormControl>
+                    <Input 
+                      placeholder="Address"
+                      {...field} 
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="col-span-4">
+            <FormField
+              control={form.control}
+              name="City"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>City</FormLabel>
+                  <FormControl>
+                    <Input 
+                      placeholder="City"
+                      type="text"
+                      {...field} 
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="col-span-4">
+            <FormField
+              control={form.control}
+              name="Province"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Province</FormLabel>
+                  <FormControl>
+                    <Input 
+                      placeholder="Province"
+                      type="text"
+                      {...field} 
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+        <FormField
+          control={form.control}
+          name="clubmembers"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>clubmembers</FormLabel>
+              <FormControl>
+                <Input 
+                  placeholder="clubmembers"
+                  {...field} 
+                />
+              </FormControl>
+              <FormDescription>Enter the ID or the related Club Member</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="familyrelationships"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>familyrelationships</FormLabel>
+              <FormControl>
+                <Input 
+                  placeholder="familyrelationships"
+                  {...field} 
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
