5351fb5cbe96d706c4285849092b0f58820356ac
EyadAbouKer <aboukereyad@gmail.com>
2025-04-04 18:12:51 -0400

    fixing Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,61 +9,47 @@ import {
 import Link from "next/link"
 
 export default function Navbar() {
-return (
-    <NavigationMenu className="p-4 flex justify-center">
-        <NavigationMenuList>
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>
-                    <Link href="/">Dashboard</Link>
-                </NavigationMenuTrigger>
-                <NavigationMenuContent>
-                    {/* <NavigationMenuLink>Entry1 Content</NavigationMenuLink> */}
-                </NavigationMenuContent>
-            </NavigationMenuItem>
+  return (
+    <NavigationMenu className="p-6 flex justify-center border-b">
+      <NavigationMenuList className="gap-2">
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="hover:bg-slate-100 transition-colors">
+            <Link href="/" className="text-slate-800 font-medium">Dashboard</Link>
+          </NavigationMenuTrigger>
+        </NavigationMenuItem>
 
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>
-                    {/* <Link href="/addentries">Add new</Link> */}
-                    Add new
-                </NavigationMenuTrigger>
-                <NavigationMenuContent className="w-64 flex justify-center">
-                    {/* <NavigationMenuLink href='/'>Club Member?</NavigationMenuLink> */}
-                    <NavigationMenuLink href='/addfamilymember'>Family Member</NavigationMenuLink>
-                    <NavigationMenuLink href='/addlocation'>Location</NavigationMenuLink>
-                    <NavigationMenuLink href='/addPayments'>Payment</NavigationMenuLink>
-                    <NavigationMenuLink href='/'>Personnel</NavigationMenuLink>
-                    <NavigationMenuLink href='/'>Personnel</NavigationMenuLink>
-                    <NavigationMenuLink href='/'>Personnel</NavigationMenuLink>
-                </NavigationMenuContent>
-            </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="hover:bg-slate-100 transition-colors">
+            <span className="text-slate-800 font-medium">Add New</span>
+          </NavigationMenuTrigger>
+          <NavigationMenuContent className="min-w-[200px] p-2 bg-white rounded-md shadow-lg">
+            <div className="flex flex-col gap-2">
+              <NavigationMenuLink href='/addfamilymember' className="text-slate-800 hover:bg-slate-100 px-4 py-2 rounded-sm transition-colors">Family Member</NavigationMenuLink>
+              <NavigationMenuLink href='/addlocation' className="text-slate-800 hover:bg-slate-100 px-4 py-2 rounded-sm transition-colors">Location</NavigationMenuLink>
+              <NavigationMenuLink href='/addPayments' className="text-slate-800 hover:bg-slate-100 px-4 py-2 rounded-sm transition-colors">Payment</NavigationMenuLink>
+              <NavigationMenuLink href='/addpersonnel' className="text-slate-800 hover:bg-slate-100 px-4 py-2 rounded-sm transition-colors">Personnel</NavigationMenuLink>
+            </div>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
 
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>
-                    <Link href="/emails_sent">Emails Sent</Link>
-                </NavigationMenuTrigger>
-                <NavigationMenuContent>
-                    {/* <NavigationMenuLink>Entry3 Content</NavigationMenuLink> */}
-                </NavigationMenuContent>
-            </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="hover:bg-slate-100 transition-colors">
+            <Link href="/emails_sent" className="text-slate-800 font-medium">Emails</Link>
+          </NavigationMenuTrigger>
+        </NavigationMenuItem>
 
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>
-                    <Link href="/">Entry4</Link>
-                </NavigationMenuTrigger>
-                <NavigationMenuContent>
-                    {/* <NavigationMenuLink>Entry4 Content</NavigationMenuLink> */}
-                </NavigationMenuContent>
-            </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="hover:bg-slate-100 transition-colors">
+            <Link href="/reports" className="text-slate-800 font-medium">Reports</Link>
+          </NavigationMenuTrigger>
+        </NavigationMenuItem>
 
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>
-                    <Link href="/">Entry5</Link>
-                </NavigationMenuTrigger>
-                <NavigationMenuContent>
-                    {/* <NavigationMenuLink>Entry5 Content</NavigationMenuLink> */}
-                </NavigationMenuContent>
-            </NavigationMenuItem>
-        </NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="hover:bg-slate-100 transition-colors">
+            <Link href="/settings" className="text-slate-800 font-medium">Settings</Link>
+          </NavigationMenuTrigger>
+        </NavigationMenuItem>
+      </NavigationMenuList>
     </NavigationMenu>
-)
+  )
 }
